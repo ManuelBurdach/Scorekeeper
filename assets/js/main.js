8aabede8b35606af9f9ac613470688946661ff06
ManuelBurdach <manuelburdach@outlook.de>
2022-04-27 12:47:48 +0200

    Final Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,26 @@
-'use strict';
-
-window.addEventListener('load', () => {
-    let homescore = document.querySelector('#homescore');
-    let awayscore = document.querySelector('#awayscore');
-
-    const reset = document.querySelector('#reset');
-
-    const button = document.querySelectorAll('#btn');
-
-    for (let i = 0; i < button.length; i++) {
-        button[i].addEventListener('click', (e) => {
-            if(button[i].getAttribute('data-team') == "home"){
-                homescore.textContent = parseInt(homescore.textContent) + parseInt(button[i].getAttribute('data-value'));
-            }else{
-                awayscore.textContent = parseInt(awayscore.textContent) + parseInt(button[i].getAttribute('data-value'));
-            }
-        });
+// Scorebutton +1 +2 +2 (Home & Away)
+    function Score(team, number){
+        if(team == "home"){
+            document.querySelector('#homescore').textContent = parseInt(document.querySelector('#homescore').textContent) + number;
+        }else{
+            document.querySelector('#awayscore').textContent = parseInt(document.querySelector('#awayscore').textContent) + number;
+        }
     }
-    
-    reset.addEventListener('click', () => {
+
+// Resetbutton (Away & Home)
+    function Reset(){
         awayscore.textContent = 0;
         homescore.textContent = 0;
-    });
-});
+    };
+
+// Resetbutton (Away & Home)
+    function Team(){
+        let home = prompt('Geben Sie ein Mannschaftsname für die Homemannschaft ein: (min 1, max 10 Buchstaben)', '');
+        if(home.length < 11 && home != ''){
+            document.querySelector('#p1').innerHTML = '&nbsp;' + home + '&nbsp;';
+        };
+        let away = prompt('Geben Sie ein Mannschaftsname für die Auswärtsmannschaft ein: (min 1, max 10 Buchstaben)', '');
+        if(away.length < 11 && away != ''){
+            document.querySelector('#p2').innerHTML = '&nbsp;' + away + '&nbsp;';
+        };
+    };
